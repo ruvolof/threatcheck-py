587b245efc071bfa352a55c5a8bce029ae83bbb3
Francesco Ruvolo <ruvolof@gmail.com>
2026-02-13 19:43:38 +0100

    Bumping version number to 0.2.0 for release.

--- src/threatcheck/__init__.py
+++ src/threatcheck/__init__.py
@@ -1,8 +1,9 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Werebug security@werebug.com'
 __license__ = 'GPL-3.0'
 
 from threatcheck.scanners.defender import DefenderScanner
 from threatcheck.scanners.amsi import AmsiScanner
+from threatcheck.scanners.clamav import ClamAVScanner
 
-__all__ = ['DefenderScanner', 'AmsiScanner']
+__all__ = ['DefenderScanner', 'AmsiScanner', 'ClamAVScanner']
